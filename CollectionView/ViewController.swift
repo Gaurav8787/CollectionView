ce5ef2f1b448d4b12bfe246ed66f1641910a1d9d
Gaurav <gaurav.purohit024@gmail.com>
2017-12-12 11:58:06 +0530

    Initial Commit

---
+++ CollectionView/ViewController.swift
@@ -0,0 +1,43 @@
+//
+//  ViewController.swift
+//  CollectionView
+//
+//  Created by Gaurav on 12/12/17.
+//  Copyright © 2017 Gaurav. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController, UICollectionViewDataSource, UICollectionViewDelegate, UICollectionViewDelegateFlowLayout {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    func numberOfSections(in collectionView: UICollectionView) -> Int {
+        return 1
+    }
+
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 10
+    }
+
+    
+     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        return CGSize(width: (UIScreen.main.bounds.size.width/2)-15, height: (UIScreen.main.bounds.size.height/2)-15)
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cell", for: indexPath)
+        cell.backgroundColor = UIColor.gray
+        return cell
+    }
+
+}
+
